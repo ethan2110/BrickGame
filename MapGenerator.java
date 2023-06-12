4b54ac795ac4b4a662ecf4822aad09ea23a708c8
ethan2110 <132939487+ethan2110@users.noreply.github.com>
2023-06-12 13:24:37 -0400

    Update MapGenerator.java

--- MapGenerator.java
+++ MapGenerator.java
@@ -4,12 +4,11 @@ import java.awt.event.*;
 
 class MapGenerator {
     
-    public int map [][];
-    public int brickWidth;
-    public int brickHeight;
+    public int map [][]; //map using array
+    public int brickWidth; //sets the width of the brick
+    public int brickHeight; //sets height of the brick
     
-    // this creates the brick of size 3x7
-    public MapGenerator(int row, int col) {
+    public MapGenerator(int row, int col) { // this creates the brick of size 3x7
         map = new int [row][col];
         for (int i = 0; i < map.length; i++) { 
             for (int j=0; j< map[0].length;j++) {
@@ -21,25 +20,24 @@ class MapGenerator {
         brickHeight = 150/row;
     }
     
-    // this draws the bricks
-    public void draw(Graphics2D g) {
+    public void draw(Graphics2D g) { // this draws the bricks
         for (int i = 0; i < map.length; i++) {
             for (int j=0; j< map[0].length;j++) {
                 if(map[i][j] > 0) {
-                    g.setColor(new Color(0XFF8787)); // brick color
-                    g.fillRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight);
+                    g.setColor(Color.RED); // brick color
+                    g.fillRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight); //fills the brick with the color
                     
                     g.setStroke(new BasicStroke(4));
-                    g.setColor(Color.BLACK);
-                    g.drawRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight);
+                    g.setColor(Color.BLACK); // brick outline color
+                    g.drawRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight); //outlines the brick with the color
                 }
             }
             
         }
     }
     
-    // this sets the value of brick to 0 if it is hit by the ball
-    public void setBrickValue(int value, int row, int col) {
+    
+    public void setBrickValue(int value, int row, int col) {// this sets the value of brick to 0 if it is hit by the ball if 0 then the brick dissapears
         map[row][col] = value;
     }
 }
