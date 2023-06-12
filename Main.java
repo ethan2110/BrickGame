b46a8be001a6721bbf27fa0e5dd0bd9c0116b634
ethan2110 <132939487+ethan2110@users.noreply.github.com>
2023-06-12 12:52:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.awt.event.*;
 
 class Main {
     public static void main(String[] args) {
-        JFrame obj = new JFrame();
-        GamePlay gamePlay = new GamePlay();
+        JFrame obj = new JFrame(); //new JFrame to see the game
+        GamePlay gamePlay = new GamePlay(); //creates new GamePlay object that intiates the game
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Brick Breaker");
         obj.setResizable(false);
